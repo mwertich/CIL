fix Kaggle wrong ScaleInv-RMSE formula (correct is minus alpha)

src/utils/loss_funcs.py
@@ -35,7 +35,7 @@ def scale_invariant_rmse(predicted, ground_truth):
     alpha = torch.mean(log_diff, dim=1, keepdim=True)
 
     # Add bias and compute RMSE
-    corrected_diff = log_diff + alpha  # Important! Add bias before squaring
+    corrected_diff = log_diff - alpha  # Important! Subtract bias before squaring (wrong on Kaggle)
     loss = torch.sqrt(torch.mean(corrected_diff ** 2, dim=1))
 
     return loss.mean() # scalar