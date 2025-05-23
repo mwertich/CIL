4327629052633bcca2d510cda1afc0b06b4fa049
elvrachor <79643898+elvrachor@users.noreply.github.com>
2025-05-23 12:47:32 +0200

    add error map visualization

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 import torchvision.transforms.functional as TF
 from pathlib import Path
@@ -12,7 +13,7 @@ def denormalize_image(tensor):
     return tensor.clamp(0, 1)
 
 
-def visualize_depth_maps(plt_title, file_name, image, pred_depth, pred_var, ground_truth=None):
+def visualize_depth_maps(plt_title, file_name, image, pred_depth, pred_var, ground_truth=None, map_error=False):
     # Convert tensors to numpy for visualization
     img_vis = denormalize_image(image.squeeze(0).cpu())
     img_np = TF.to_pil_image(img_vis)
@@ -25,16 +26,25 @@ def visualize_depth_maps(plt_title, file_name, image, pred_depth, pred_var, grou
     if ground_truth is not None:
         depth_np = ground_truth.squeeze().cpu().numpy()
         gt_disp = (depth_np - depth_np.min()) / (depth_np.max() - depth_np.min())
-        captions = ["Input Image", "Uncertainty", "Ground Truth", "Predicted Depth"]
-        plot_images = [img_np, pred_var_disp, gt_disp, pred_depth_disp]
-        cmaps = [None, "viridis", "plasma", "plasma"]
+        captions = ["Input Image", "Ground Truth", "Predicted Depth", "Uncertainty"]
+        plot_images = [img_np, gt_disp, pred_depth_disp, pred_var_disp]
+        cmaps = [None, "plasma", "plasma", "viridis"]
     else:
-        captions = ["Input Image", "Uncertainty", "Predicted Depth"]
-        plot_images = [img_np, pred_var_disp, pred_depth_disp]
-        cmaps = [None, "viridis", "plasma"]
+        captions = ["Input Image", "Predicted Depth", "Uncertainty"]
+        plot_images = [img_np, pred_depth_disp, pred_var_disp]
+        cmaps = [None, "plasma", "viridis"]
+
+    if map_error:
+        assert ground_truth is not None
+
+        error_np = np.abs(pred_depth_np - depth_np)
+        erorr_disp = (error_np - error_np.min()) / (error_np.max() - error_np.min())
+        captions.append("Error (absolute value)")
+        plot_images.append(erorr_disp)
+        cmaps.append('viridis')
     
     # Plot
-    ncols = 3 if ground_truth is None else 4
+    ncols = 5 if map_error else 4 if ground_truth is not None else 3
 
     fig, axs = plt.subplots(1, ncols, figsize=(12, 4))
     fig.suptitle(plt_title)
@@ -51,7 +61,7 @@ def visualize_depth_maps(plt_title, file_name, image, pred_depth, pred_var, grou
     plt.close(fig)
 
 
-def visualize_prediction_with_ground_truth(model, loader, run_id, image_size, device, num_images=5):
+def visualize_prediction_with_ground_truth(model, loader, run_id, image_size, device, num_images=5, map_error=False):
     model.eval()
 
     out_dir = Path(f'./depth_maps/val/{run_id}')
@@ -75,7 +85,7 @@ def visualize_prediction_with_ground_truth(model, loader, run_id, image_size, de
             for image, depth, pred_depth, pred_var in zip(images, depths, pred_depths_resized, pred_vars_resized): 
                 images_shown += 1
                 file_name = f"depth_maps/val/{run_id}/midas_uq_depth_map_{images_shown}.png"
-                visualize_depth_maps("Depths Map Validation Set", file_name, image, pred_depth, pred_var, depth)
+                visualize_depth_maps("Depths Map Validation Set", file_name, image, pred_depth, pred_var, depth, map_error)
                 # file_name = f"depth_maps/val/midas_uq_var_map_{images_shown}.png"
                 # visualize_depth_maps("Uncertainty Map Validation Set", file_name, image, pred_var, uncertainty=True)
                 if images_shown >= num_images:
